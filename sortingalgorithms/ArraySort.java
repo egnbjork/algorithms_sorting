f8493ac33bcd9b2887aa2d3de49515b6851b696d
Eugene Berberyan <eugene.berberyan@dataart.com>
2021-02-26 12:30:38 +0200

    arraysort interface changed

--- sortingalgorithms/ArraySort.java
+++ sortingalgorithms/ArraySort.java
@@ -1,10 +1,19 @@
 package sortingalgorithms;
 
+import helpers.*;
+
 public interface ArraySort {
 
     int[] sort(int arr[]);
 
-    long getBenchmark(int arraySize);
-
     String getSortName();
+
+    default long getBenchmark(int arraySize) {
+        int[] randomNumbers = RandomNumbersGenerator.generate(arraySize);
+        long start = System.currentTimeMillis();
+        int[] sortedArray = sort(randomNumbers);
+        long stop = System.currentTimeMillis();
+        TestSorted.test(sortedArray, false);
+        return stop - start;
+    }
 }
--- sortingalgorithms/BubbleSort.java
+++ sortingalgorithms/BubbleSort.java
@@ -24,18 +24,6 @@ public class BubbleSort implements ArraySort {
     }
 
     @Override
-    public long getBenchmark(int arraySize) {
-        int[] randomNumbers = RandomNumbersGenerator.generate(arraySize);
-        long start = System.currentTimeMillis();
-
-        int[] sortedArray = sort(randomNumbers);
-
-        long stop = System.currentTimeMillis();
-        TestSorted.test(sortedArray, false);
-        return stop - start;
-    }
-
-    @Override
     public String getSortName() {
         return "Bubble Sort";
     }
